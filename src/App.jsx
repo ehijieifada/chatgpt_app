d328610ee0a0b063174818e4df44692586954d43
ifadaehijie <sentbliss23@gmail.com>
2023-10-31 12:37:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useState } from 'react'
+import './index.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+
+const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+const systemMessage = { 
+  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
+}
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm here to help you!",
+      sentTime: "just now",
+      sender: "ChatGPT"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
+    };
+
+    const newMessages = [...messages, newMessage];
+    
+    setMessages(newMessages);
+
+    
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,  
+        ...apiMessages 
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + apiKey,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+      }]);
+      setIsTyping(false);
+    });
+  }
+
+  return (
+    <div className="App">
+      <div className="main">
+        <MainContainer>
+          <ChatContainer>       
+            <MessageList style={{ display: "block", background:"linear-gradient(90deg, rgba(46, 50, 49, 1) 0%, rgba(28, 36, 66, 1) 54%, rgba(28, 28, 30, 1) 100%)"  }} 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="Your Response is here" /> : null}
+            >
+              {messages.map((message, i) => {
+                console.log(message)
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput style={{ backgroundColor:"darkblue", width: "100%" }} placeholder="Ask AI" onSend={handleSend} />        
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    
+      <div id="info">Chat GPT Oct 12 Version. Free Research Preview. Our goal is to make AI systems more natural and safe to interact with. Your feedback will help us improve
+      </div>
+
+    </div>
+  )
+}
+
+export default App
